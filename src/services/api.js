97628c7794770c8ab223ecae5f90bda4b9e0f473
MadhaviGit0607 <162568991+MadhaviGit0607@users.noreply.github.com>
2024-09-08 18:34:54 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = 'e31ebff5b65ca0696cbff1adcd49393f'; // Replace with your actual API key
+const API_KEY = 'e31ebff5b65ca0696cbff1adcd49393f';
 const API_URL = 'https://public.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000/api/';
 
 export const fetchCities = async (page = 1) => {
@@ -14,13 +14,8 @@ export const fetchCities = async (page = 1) => {
   }
 };
 
-
-  
-
-  // src/services/api.js
-
 export const fetchWeather = async (cityId) => {
-    const apiKey = 'e31ebff5b65ca0696cbff1adcd49393f'; // Replace with your actual API key
+    const apiKey = 'e31ebff5b65ca0696cbff1adcd49393f';
     const url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=metric`;
   
     try {
@@ -31,7 +26,7 @@ export const fetchWeather = async (cityId) => {
       return await response.json();
     } catch (error) {
       console.error('Error fetching weather data:', error);
-      throw error; // Re-throw the error to handle it in the component
+      throw error;
     }
   };
   
